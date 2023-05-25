b5c98f2c0614de15e395ebb9503c7a8af1fb00ce
Snibaw <Snibaw@gmail.com>
2023-05-25 14:52:22 +0200

    Add main menu

---
+++ Fireman-Nonsense/Assets/MainMenuManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsMenu;
+
+    private void Start()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+    public void LevelSelector()
+    {
+        SceneManager.LoadScene("LevelSelector");
+    }
+    public void MoreGames()
+    {
+        Application.OpenURL("https://itch.io/profile/snibaw");
+    }
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true);
+    }
+    public void SaveSettings()
+    {
+        settingsMenu.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void Test()
+    {
+        Debug.Log("Test");
+    }
+}
