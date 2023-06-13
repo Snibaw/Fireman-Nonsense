aed52943cb388b2be05f4de07c4567144a672500
Snibaw <Snibaw@gmail.com>
2023-06-13 15:05:44 +0200

    Add wall on fire

---
+++ Fireman-Nonsense/Assets/WallFlame.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFlame : MonoBehaviour
+{
+    [SerializeField] private float damageToPlayer = 300f;
+    private void OnTriggerEnter(Collider other) {
+        if (other.name == "Player")
+        {
+            other.GetComponent<playerInput>().ChangeCurrentMana(-damageToPlayer);
+            other.transform.position = new Vector3(other.transform.position.x,other.transform.position.y,other.transform.position.z+1);
+        }
+    }
+}
